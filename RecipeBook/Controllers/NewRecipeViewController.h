df5a379fca5ce4dfe32acb9b855fee9dd97b964f
Morgan Lieberthal <morgan@jmorgan.org>
2016-01-07 16:31:00 -0700

    Begin creating UI for adding new recipes

---
+++ RecipeBook/Controllers/NewRecipeViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewRecipeViewController.h
+//  RecipeBook
+//
+//  Created by Morgan Lieberthal on 1/7/16.
+//  Copyright © 2016 Morgan Lieberthal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NewRecipeViewController : NSViewController
+
+@end
---
+++ RecipeBook/Controllers/NewRecipeViewController.m
@@ -0,0 +1,22 @@
+//
+//  NewRecipeViewController.m
+//  RecipeBook
+//
+//  Created by Morgan Lieberthal on 1/7/16.
+//  Copyright © 2016 Morgan Lieberthal. All rights reserved.
+//
+
+#import "NewRecipeViewController.h"
+
+@interface NewRecipeViewController ()
+
+@end
+
+@implementation NewRecipeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
