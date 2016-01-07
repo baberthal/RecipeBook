889a9a274c05a2a34481c51cae87db0ff3f59936
Morgan Lieberthal <morgan@jmorgan.org>
2016-01-06 18:05:41 -0700

    Initial Commit

---
+++ RecipeBook/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  RecipeBook
+//
+//  Created by Morgan Lieberthal on 1/6/16.
+//  Copyright © 2016 Morgan Lieberthal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
