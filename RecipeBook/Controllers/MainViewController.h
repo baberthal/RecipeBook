de2526d1e92b053d1f460e346173c3a4b54d6658
Morgan Lieberthal <morgan@jmorgan.org>
2016-01-06 19:53:24 -0700

    Add Main ViewController

---
+++ RecipeBook/Controllers/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  RecipeBook
+//
+//  Created by Morgan Lieberthal on 1/6/16.
+//  Copyright © 2016 Morgan Lieberthal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainViewController : NSViewController <NSOutlineViewDataSource, NSOutlineViewDelegate>
+
+@end
---
+++ RecipeBook/Controllers/MainViewController.m
@@ -0,0 +1,22 @@
+//
+//  MainViewController.m
+//  RecipeBook
+//
+//  Created by Morgan Lieberthal on 1/6/16.
+//  Copyright © 2016 Morgan Lieberthal. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
