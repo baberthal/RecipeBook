0905ea751a5fcd2f388eb67c4882bb90511a34e2
Morgan Lieberthal <morgan@jmorgan.org>
2016-01-06 18:45:45 -0700

    Add main window controller

---
+++ RecipeBook/Controllers/MainWindowController.m
@@ -0,0 +1,26 @@
+//
+//  MainWindowController.m
+//  RecipeBook
+//
+//  Created by Morgan Lieberthal on 1/6/16.
+//  Copyright © 2016 Morgan Lieberthal. All rights reserved.
+//
+
+#import "MainWindowController.h"
+
+@interface MainWindowController ()
+
+@end
+
+@implementation MainWindowController
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+}
+
+- (void)awakeFromNib
+{
+    NSLog(@"Window loaded.");
+}
+@end
