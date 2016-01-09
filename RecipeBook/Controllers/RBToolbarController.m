3c3cf8587a0a385104c4fb43b3c80569b213bd50
Morgan Lieberthal <morgan@jmorgan.org>
2016-01-09 16:52:54 -0700

    Rename MainWindowController
    New name more accurately represents the object's purpose.

---
+++ RecipeBook/Controllers/RBToolbarController.m
@@ -0,0 +1,22 @@
+//
+//  MainWindowController.m
+//  RecipeBook
+//
+//  Created by Morgan Lieberthal on 1/6/16.
+//  Copyright © 2016 Morgan Lieberthal. All rights reserved.
+//
+
+#import "RBToolbarController.h"
+
+@interface RBToolbarController ()
+
+@end
+
+@implementation RBToolbarController
+
+- (void)awakeFromNib
+{
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+@end
