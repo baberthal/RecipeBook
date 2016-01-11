647777400cfec5b6840e00637db21fad29fad8bd
Morgan Lieberthal <morgan@jmorgan.org>
2016-01-11 15:09:55 -0700

    Begin adding definitions for data types

---
+++ RecipeBook/RBDefinitions.h
@@ -0,0 +1,11 @@
+//
+//  RBDefinitions.h
+//  RecipeBook
+//
+//  Created by Morgan Lieberthal on 1/11/16.
+//  Copyright © 2016 Morgan Lieberthal. All rights reserved.
+//
+
+#pragma once
+
+#define kRBRecipeStepDataType @"kRBRecipeStepDataType"
